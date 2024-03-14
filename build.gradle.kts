bbe7af1eaa11b496f22e917ba58ff8565a21c7eb
voroninmms <msvoronin_1@edu.hse.ru>
2024-03-14 21:54:17 +0300

    fixed dep issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     kotlin("jvm") version "1.9.21"
+    application
+}
+application {
+    mainClass.set("MainKt")
 }
 
 group = "org.example"
@@ -11,6 +15,7 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    implementation("mysql:mysql-connector-java:8.0.29")
 }
 
 tasks.test {
